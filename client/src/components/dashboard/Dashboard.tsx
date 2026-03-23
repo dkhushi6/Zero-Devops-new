14060f531feabb4997a4d56b373caefb9a1b6868
Khushi Dhankhar <khushidhankhar006@gmail.com>
2026-03-23 19:16:19 +0530

    feat:ui updation login & dashboard page

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,5 +1,14 @@
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <div className="text-9xl font-bold tracking-tight leading-none text-center">
+        <span className="text-black">Deploy faster.</span>{" "}
+        <span className="text-gray-400">Scale smarter.</span>{" "}
+        <span className="text-black">Ship with</span>{" "}
+        <span className="text-gray-400">confidence.</span>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
